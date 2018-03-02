10f8aa83116ceb1faa985e60afb6066940a9ccb3
Peter WONG <luiges90@gmail.com>
2018-03-02 21:52:06 +0800

    修正運算錯誤

--- WorldOfTheThreeKingdoms/GameObjects/Person.cs
+++ WorldOfTheThreeKingdoms/GameObjects/Person.cs
@@ -2692,7 +2692,7 @@ namespace GameObjects
             }
             if (q.Age > 40 + (q.Sex ? 0 : 10))
             {
-                extraRate -= (q.Age - 40 - (this.Sex ? 0 : 10)) / 10.0f;
+                extraRate -= (q.Age - 40 - (q.Sex ? 0 : 10)) / 10.0f;
             }
             if (this.Age < 16)
             {
