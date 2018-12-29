5fa1ed26a37e9c0a47992e8bbbe816e0145aed48
Peter WONG <luiges90@gmail.com>
2018-12-29 09:26:51 +0800

    調整子女AI培育數

--- WorldOfTheThreeKingdoms/GameObjects/GameScenario.cs
+++ WorldOfTheThreeKingdoms/GameObjects/GameScenario.cs
@@ -5860,7 +5860,7 @@ namespace GameObjects
             foreach (Person p in this.Persons)
             {
                 //if (p.Trainable && GameObject.Random(30) == 0)
-                if (p.Trainable && GameObject.Random(20 / Session.Parameters.DayInTurn) == 0)
+                if (p.Trainable && GameObject.Random((int)(20 / Session.Current.Scenario.Parameters.AIExtraPerson / Session.Parameters.DayInTurn)) == 0)
                 {
                     if (p.TrainPolicy == null)
                     {
