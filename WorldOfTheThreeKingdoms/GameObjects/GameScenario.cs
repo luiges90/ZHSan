789f203e7d41e7857e828944df425656adaca2e0
Peter WONG <luiges90@gmail.com>
2018-12-31 21:25:05 +0800

    修正錯誤

--- WorldOfTheThreeKingdoms/GameObjects/GameScenario.cs
+++ WorldOfTheThreeKingdoms/GameObjects/GameScenario.cs
@@ -5860,7 +5860,7 @@ namespace GameObjects
             foreach (Person p in this.Persons)
             {
                 //if (p.Trainable && GameObject.Random(30) == 0)
-                if (p.Trainable && GameObject.Random((int)(20 / Session.Current.Scenario.Parameters.AIExtraPerson / Session.Parameters.DayInTurn)) == 0)
+                if (p.Trainable && GameObject.Random((int)(20 / (IsPlayer(p.BelongedFaction) ? 1 : Session.Current.Scenario.Parameters.AIExtraPerson) / Session.Parameters.DayInTurn)) == 0)
                 {
                     if (p.TrainPolicy == null)
                     {
