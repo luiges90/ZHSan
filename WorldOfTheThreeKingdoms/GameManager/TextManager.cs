6d8e035c5e52d3685918118f77f73cefdec2ebe7
Peter WONG <luiges90@gmail.com>
2019-02-24 22:56:46 +0800

    更新文字

--- WorldOfTheThreeKingdoms/GameManager/TextManager.cs
+++ WorldOfTheThreeKingdoms/GameManager/TextManager.cs
@@ -33,9 +33,12 @@ namespace GameManager
 
         private const float LITTLEWIDTHSCALE = 0.45f;
 
-        private static char[] SMALLCHARS = new char[] { '，', '。', '、', '-', '“', '”', '"', ' ', '（', '）' };
+        private static char[] SMALLCHARS = new char[] { '，', '。', '、', '“', '”', '"', ' ', '（', '）' };
 
-        private static char[] LITTLECHARS = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        private static char[] LITTLECHARS = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+            ',', '(', ')', '*', '-', ':', ';', '[', ']', '/'};
 
 
         private static Object LockObject = new object();
