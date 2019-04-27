497c3a92a7e0ca4261893ccebfb6e427d8d81b7d
lemingcen <leming.cen@live.com>
2019-04-27 22:12:26 +0800

    安卓版系列更新

--- WorldOfTheThreeKingdoms.Play/Properties/AssemblyInfo.cs
+++ WorldOfTheThreeKingdoms.Play/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WorldOfTheThreeKingdoms.Pipeline")]
-[assembly: AssemblyProduct("WorldOfTheThreeKingdoms.Pipeline")]
+[assembly: AssemblyTitle("WorldOfTheThreeKingdoms.Android")]
+[assembly: AssemblyProduct("WorldOfTheThreeKingdoms.Android")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
@@ -20,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cf57131e-7bd4-46e4-8eb3-a04486980b3b")]
+[assembly: Guid("025bbfe5-413f-4a4e-8fe2-61019517917f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,3 +35,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Add some common permissions, these can be removed if not needed
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
