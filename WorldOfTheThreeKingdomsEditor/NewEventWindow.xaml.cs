a83fafd24f98fd2c2747adfa1fd4a0a8914398b7
Peter WONG <luiges90@gmail.com>
2019-10-24 19:14:09 +0800

    新增事件編輯窗

--- WorldOfTheThreeKingdomsEditor/NewEventWindow.xaml.cs
+++ WorldOfTheThreeKingdomsEditor/NewEventWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +21,59 @@ namespace WorldOfTheThreeKingdomsEditor
     /// </summary>
     public partial class NewEventWindow : Window
     {
+        private GameScenario scen;
+        private bool edit = false;
+
+        private Event e;
+
         public NewEventWindow(bool editing, DataGrid dataGrid, GameScenario scen)
         {
+            this.scen = scen;
+            edit = editing;
             InitializeComponent();
+
+            e = new Event();
+
+            if (editing)
+            {
+                e = scen.AllEvents.GetGameObject(int.Parse(((DataRowView)dataGrid.SelectedItem).Row["ID"].ToString())) as Event;
+            }
+
+            txname.Text = e.Name;
+            cbHappened.IsChecked = e.happened;
+            cbRepeatable.IsChecked = e.repeatable;
+            cbMinor.IsChecked = e.Minor;
+            cbGloballyDisplayed.IsChecked = e.GloballyDisplayed;
+            tbHappenChance.Text = e.happenChance.ToString();
+            tbStartYear.Text = e.StartYear.ToString();
+            tbStartMonth.Text = e.StartMonth.ToString();
+            tbEndYear.Text = e.EndYear.ToString();
+            tbEndMonth.Text = e.EndMonth.ToString();
+        }
+
+        private void BtSave_Click(object sender, RoutedEventArgs rea)
+        {
+            e.Name = txname.Text;
+            e.happened = cbHappened.IsChecked.GetValueOrDefault(false);
+            e.repeatable = cbRepeatable.IsChecked.GetValueOrDefault(false);
+            e.Minor = cbMinor.IsChecked.GetValueOrDefault(false);
+            e.GloballyDisplayed = cbGloballyDisplayed.IsChecked.GetValueOrDefault(false);
+            int.TryParse(tbHappenChance.Text, out e.happenChance);
+            int.TryParse(tbStartYear.Text, out e.StartYear);
+            int.TryParse(tbStartMonth.Text, out e.StartMonth);
+            int.TryParse(tbEndYear.Text, out e.EndYear);
+            int.TryParse(tbEndMonth.Text, out e.EndMonth);
+
+            if (!edit)
+            {
+                scen.AllEvents.Add(e);
+            }
+            this.Close();
+        }
+
+        private void BtExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
