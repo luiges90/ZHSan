0470a5a1d3d42960f4ebca248d21aa67c009dc9c
Peter WONG <luiges90@gmail.com>
2019-10-24 22:53:04 +0800

    填入事件資料

--- WorldOfTheThreeKingdomsEditor/NewEventWindow.xaml.cs
+++ WorldOfTheThreeKingdomsEditor/NewEventWindow.xaml.cs
@@ -49,6 +49,52 @@ namespace WorldOfTheThreeKingdomsEditor
             tbStartMonth.Text = e.StartMonth.ToString();
             tbEndYear.Text = e.EndYear.ToString();
             tbEndMonth.Text = e.EndMonth.ToString();
+            tbImage.Text = e.Image;
+            tbSound.Text = e.Sound;
+
+            PopulatePersonData(0, lblPerson0, lblPersonCond0, lblEffect0, lblBiography0, lblYesEffect0, lblNoEffect0);
+            PopulatePersonData(1, lblPerson1, lblPersonCond1, lblEffect1, lblBiography1, lblYesEffect1, lblNoEffect1);
+            PopulatePersonData(2, lblPerson2, lblPersonCond2, lblEffect2, lblBiography2, lblYesEffect2, lblNoEffect2);
+            PopulatePersonData(3, lblPerson3, lblPersonCond3, lblEffect3, lblBiography3, lblYesEffect3, lblNoEffect3);
+            PopulatePersonData(4, lblPerson4, lblPersonCond4, lblEffect4, lblBiography4, lblYesEffect4, lblNoEffect4);
+            PopulatePersonData(5, lblPerson5, lblPersonCond5, lblEffect5, lblBiography5, lblYesEffect5, lblNoEffect5);
+
+            lblArchitcture.Content = String.Join(" ", e.architecture.GameObjects.Select(p => p.Name));
+            lblArchitctureCond.Content = String.Join(" ", e.architectureCond.Select(p => p.Name));
+            lblArchitctureEffect.Content = String.Join(" ", e.architectureEffect.Select(p => p.Name));
+            lblArchitctureYesEffect.Content = String.Join(" ", e.yesArchitectureEffect.Select(p => p.Name));
+            lblArchitctureNoEffect.Content = String.Join(" ", e.noArchitectureEffect.Select(p => p.Name));
+            lblFaction.Content = String.Join(" ", e.faction.GameObjects.Select(p => p.Name));
+            lblFactionCond.Content = String.Join(" ", e.factionCond.Select(p => p.Name));
+            lblFactionEffect.Content = String.Join(" ", e.factionEffect.Select(p => p.Name));
+        }
+
+        private void PopulatePersonData(int index, Label person, Label personCond, Label effect, Label biography, Label yesEffect, Label noEffect)
+        {
+            if (e.person.ContainsKey(index))
+            {
+                person.Content = String.Join(" ", e.person[index].Select(p => p == null ? "任何" : p.Name));
+            }
+            if (e.personCond.ContainsKey(index))
+            {
+                personCond.Content = String.Join(" ", e.personCond[index].Select(c => c.Name));
+            }
+            if (e.effect.ContainsKey(index))
+            {
+                effect.Content = String.Join(" ", e.effect[index].Select(e => e.Name));
+            }
+            if (e.scenBiography.Count > index)
+            {
+                biography.Content = e.scenBiography[index];
+            }
+            if (e.yesEffect.ContainsKey(index))
+            {
+                yesEffect.Content = e.yesEffect[index];
+            }
+            if (e.noEffect.ContainsKey(index))
+            {
+                noEffect.Content = e.noEffect[index];
+            }
         }
 
         private void BtSave_Click(object sender, RoutedEventArgs rea)
@@ -63,6 +109,8 @@ namespace WorldOfTheThreeKingdomsEditor
             int.TryParse(tbStartMonth.Text, out e.StartMonth);
             int.TryParse(tbEndYear.Text, out e.EndYear);
             int.TryParse(tbEndMonth.Text, out e.EndMonth);
+            e.Image = tbImage.Text;
+            e.Sound = tbSound.Text;
 
             if (!edit)
             {
