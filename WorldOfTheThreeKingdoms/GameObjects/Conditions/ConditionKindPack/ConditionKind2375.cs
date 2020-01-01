0f9146c599f697835c190611a5420afbb9a41150
Peter WONG <luiges90@gmail.com>
2020-01-01 15:46:52 +0800

    新增設施興建天數條件

---
+++ WorldOfTheThreeKingdoms/GameObjects/Conditions/ConditionKindPack/ConditionKind2375.cs
@@ -0,0 +1,31 @@
+﻿using GameObjects;
+using GameObjects.Conditions;
+using System;
+
+
+using System.Runtime.Serialization;namespace GameObjects.Conditions.ConditionKindPack
+{
+
+    [DataContract]public class ConditionKind2375 : ConditionKind
+    {
+        private int val;
+
+        public override bool CheckConditionKind(Architecture a)
+        {
+            return a.BuildingDaysLeft < val;
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.val = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+
+    }
+}
+
