7545b828f26bd8971bcb12fceb0bfd8b85d4a353
Peter WONG <luiges90@gmail.com>
2024-02-21 21:25:01 +0800

    新增設施武將修練影響

---
+++ WorldOfTheThreeKingdoms/GameObjects/Influences/InfluenceKindPack/InfluenceKind3502.cs
@@ -0,0 +1,40 @@
+﻿using GameObjects;
+using GameObjects.Influences;
+using System;
+
+
+using System.Runtime.Serialization;namespace GameObjects.Influences.InfluenceKindPack
+{
+
+    [DataContract]public class InfluenceKind3502 : InfluenceKind
+    {
+        private float rate = 1f;
+
+        public override void ApplyInfluenceKind(Architecture architecture)
+        {
+            
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.rate = float.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+
+        public override void PurifyInfluenceKind(Architecture architecture)
+        {
+
+        }
+
+        public override double AIFacilityValue(Architecture a)
+        {
+            return 1;
+        }
+    }
+}
+
